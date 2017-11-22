9e0f466866822f14face46b086c99173a38f1b02
JoePascale <joe.m.pascale@gmail.com>
2017-11-22 13:23:00 -0800

    updated styling with bootswatch-rails gem & filled out form and item partials for users#show page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,21 +1,17 @@
 class ItemsController < ApplicationController
-  def create(name)
-    @item = Item.new(item_params)
-    @item.name = name
-    @item.user = current_user
-
-    if item.save
+  def create
+    @item = current_user.items.new(item_params)
+    if @item.save
       flash[:notice] = "You created a new To-Do item."
-      redirect_to root_path
     else
       flash.now[:alert] = "There was an error saving your new To-Do item."
-      redirect_to root_path
     end
+    redirect_to root_path
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:name, :user)
+    params.require(:item).permit(:name)
   end
 end
